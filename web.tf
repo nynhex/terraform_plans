caebcc4887c49b5c2064e49ec5d191c15695fca3
nynhex <nynhex@gmail.com>
2018-04-08 20:46:07 -0500

    Adds remote-exec and connection to install nginx on each node

--- web.tf
+++ web.tf
@@ -1,21 +1,49 @@
-resource "digitalocean_droplet" "disruptsocialnode1" {
+resource "digitalocean_droplet" "node1" {
     image = "ubuntu-16-04-x64"
-    name = "node1.disrupsocial.network"
+    name = "node1"
     region = "nyc3"
     size = "4gb"
     private_networking = true
     ssh_keys = [
      "${var.ssh_fingerprint}"
      ]
-    }
+ provisioner "remote-exec" {
+   inline = [
+     "export PATH=$PATH:/usr/bin",
+     # install nginx
+     "sudo apt-get update",
+     "sudo apt-get -y install nginx"
+   ]
+   connection {
+       user = "root"
+       type = "ssh"
+       private_key = "${file(var.pvt_key)}"
+       timeout = "2m"
+   }
+ }
+}
 
-resource "digitalocean_droplet" "disruptsocialnode2" {
+resource "digitalocean_droplet" "node2" {
     image = "ubuntu-16-04-x64"
-    name = "node2.disrupsocial.network"
+    name = "node2"
     region = "nyc3"
     size = "4gb"
     private_networking = true
     ssh_keys = [
      "${var.ssh_fingerprint}"
      ]
+ provisioner "remote-exec" {
+   inline = [
+     "export PATH=$PATH:/usr/bin",
+     # install nginx
+     "sudo apt-get update",
+     "sudo apt-get -y install nginx"
+   ]
+   connection {
+       user = "root"
+       type = "ssh"
+       private_key = "${file(var.pvt_key)}"
+       timeout = "2m"
+   }
+ }
     }
